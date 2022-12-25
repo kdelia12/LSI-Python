37fa8cbab8cccb7db8a43fc26690e146ad718f14
kdelia12 <wild12748@gmail.com>
2022-12-25 22:47:55 +0700

    menambahkan tulisan dokumen kosong dan tidak dapat melanjutkan proses

--- main.py
+++ main.py
@@ -6,19 +6,22 @@ from sklearn.metrics.pairwise import cosine_similarity
 # List dokumen yang akan diindeks
 documents = [
 ]
+if not documents:
+    print("Tidak ada dokumen yang diindeks")
+else:
 
 # Buat matriks dokumen-term dengan menggunakan TfidfVectorizer
-vectorizer = TfidfVectorizer()
-X = vectorizer.fit_transform(documents)
+    vectorizer = TfidfVectorizer()
+    X = vectorizer.fit_transform(documents)
 
 # Hitung SVD dari matriks dokumen-term
-lsa = TruncatedSVD(n_components=2)
-X_lsa = lsa.fit_transform(X)
+    lsa = TruncatedSVD(n_components=2)
+    X_lsa = lsa.fit_transform(X)
 
 # Indeks LSI
-index = {}
-for i, document in enumerate(documents):
-    index[document] = X_lsa[i]
+    index = {}
+    for i, document in enumerate(documents):
+        index[document] = X_lsa[i]
 
 # Pencarian
 def search(query):
@@ -41,22 +44,28 @@ def search(query):
     return results
 
 #sediakan input dan panggil fungsi search
-query = input("Masukkan query: ")
-results = search(query)
-print("Hasil pencarian: ")
-for result in results:
-    print(result)
+if not documents:
+    print("Tidak ada dokumen yang diindeks")
+else:
+    query = input("Masukkan query: ")
+    results = search(query)
+    print("Hasil pencarian: ")
+    for result in results:
+        print(result)
 
 #sediakan pilihan untuk mengulang pencarian
-while True:
-    ulang = input("Apakah anda ingin mencari lagi? (y/n): ")
-    if ulang == "y":
-        query = input("Masukkan query: ")
-        results = search(query)
-        print("Hasil pencarian: ")
-        for result in results:
-            print(result)
-    elif ulang == "n":
-        break
-    else:
-        print("Input tidak valid")
+    while True:
+        ulang = input("Apakah anda ingin mencari lagi? (y/n): ")
+        if ulang == "y":
+            query = input("Masukkan query: ")
+            results = search(query)
+            print("Hasil pencarian: ")
+            for result in results:
+                print(result)
+        elif ulang == "n":
+            break
+        else:
+            print("Input tidak valid")
+
+#jika document kosong, maka akan muncul pesan error, jika tidak maka akan muncul hasil pencarian
+
