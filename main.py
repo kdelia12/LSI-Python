d555883be94ba7058272e837566a6bc41a3943d0
kdelia12 <wild12748@gmail.com>
2022-12-26 00:49:22 +0700

    adding web features

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
 from sklearn.metrics.pairwise import cosine_similarity
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+#route untuk menampilkan halaman utama dalam folder template dengan nama index.html
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 
 
 # List dokumen yang akan diindeks
@@ -8,22 +17,19 @@ documents = [
     "aubrey adalah orang yang cantik",
     "wildan adalah orang yang gateng"
 ]
-if not documents:
-    print("Tidak ada dokumen yang diindeks")
-else:
 
 # Buat matriks dokumen-term dengan menggunakan TfidfVectorizer
-    vectorizer = TfidfVectorizer()
-    X = vectorizer.fit_transform(documents)
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(documents)
 
 # Hitung SVD dari matriks dokumen-term
-    lsa = TruncatedSVD(n_components=2)
-    X_lsa = lsa.fit_transform(X)
+lsa = TruncatedSVD(n_components=2)
+X_lsa = lsa.fit_transform(X)
 
 # Indeks LSI
-    index = {}
-    for i, document in enumerate(documents):
-        index[document] = X_lsa[i]
+index = {}
+for i, document in enumerate(documents):
+    index[document] = X_lsa[i]
 
 # Pencarian
 def search(query):
@@ -43,31 +49,23 @@ def search(query):
             results.append(document)
     # Urutkan hasil pencarian berdasarkan skor
     results.sort(key=lambda x: scores[x], reverse=True)
+    # Jika result kosong, tambahkan dokumen dengan skor diatas 0.3
+    if not results:
+        for document, score in scores.items():
+            if score > 0.3:
+                results.append(document)
+    #jika masih kosong, Tambahkan Result not found
+    if not results:
+        results.append("Result not found")
+    # Kembalikan hasil pencarian
     return results
 
-#sediakan input dan panggil fungsi search
-if not documents:
-    print("Tidak ada dokumen yang diindeks")
-else:
-    query = input("Masukkan query: ")
-    results = search(query)
-    print("Hasil pencarian: ")
-    for result in results:
-        print(result)
+@app.route('/result', methods=['POST'])
+def result():
+    if request.method == 'POST':
+        query = request.form['query']
+        results = search(query)
+        return render_template('index.html', results=results)
 
-#sediakan pilihan untuk mengulang pencarian
-    while True:
-        ulang = input("Apakah anda ingin mencari lagi? (y/n): ")
-        if ulang == "y":
-            query = input("Masukkan query: ")
-            results = search(query)
-            print("Hasil pencarian: ")
-            for result in results:
-                print(result)
-        elif ulang == "n":
-            break
-        else:
-            print("Input tidak valid")
-
-#jika document kosong, maka akan muncul pesan error, jika tidak maka akan muncul hasil pencarian
-
+if __name__ == "__main__":
+    app.run(debug=True)
